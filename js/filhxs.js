d54a9a4e791929fd2ef9cb720b68839ac8e1f46c
Murilo Costa Gonçalves <murilo.costa@sga.pucminas.br>
2018-06-18 12:30:23 -0300

    Preparando integração com Web Service dos Filhxs

--- js/filhxs.js
+++ js/filhxs.js
@@ -3,25 +3,70 @@ $(document).ready(function () {
     findFilhx("-LFF6YTtcLBrb3kzHNuH", function (snapshot) {
         filhx = snapshot.val();
         loadInfoUser();
+        loadInfoSprint();
         renderTasks();
     });
 });
 
-function iniciarSprint() {
-    
+function changeStateSprint() {
+    if ($("#change-state-sprint i").hasClass("fa-play")) {
+        startSprint(filhx.key);
+        $("#change-state-sprint").attr("class", "btn-sprint pattern-color-red");
+        $("#change-state-sprint i").attr("class", "fa fa-stop fa-2x");
+    } else {
+        stopSprint(filhx.key);
+        $("#change-state-sprint").attr("class", "btn-sprint pattern-color-green");
+        $("#change-state-sprint i").attr("class", "fa fa-play fa-2x");
+    }
 }
 
-function salvarComentario() {
-
+function addComentario() {
+    var atividade = $("#atividade-key").val();
+    var description = $("#comentario").val();
+    var comentario = {
+        key: 0,
+        comentario: description,
+        atividade: atividade
+    }
+    salvarComentario(comentario);
 }
 
-function sendTask(key) {
-    findAtividade(key, function(snapshot){
-       var atividade = snapshot.val();
+function sendTask() {
+    var key = $("#confirm-send-task-atividade").val();
+    findAtividade(key, function (snapshot) {
+        var atividade = snapshot.val();
         atividade.estado = 1;
-        salvarAtividade(atividade, function(result){
-           $("#task_" + key + " button.btn.sendButton").attr("disabled", "disabled");
+        salvarAtividade(atividade, function (result) {
+            $("#task_" + key + " button.btn.sendButton").attr("disabled", "disabled");
+            $("#task_" + key + " .option-task").attr("disabled", "disabled");
+            $("#task_" + key).css("background-color", "#f9f9f9");
         });
+    });
+}
+
+
+
+function openModalComentario(key) {
+    $("#atividade-key").val(key);
+    $("#modal-comentario").modal("show");
+}
+
+function openModalConfirmSendTask(key) {
+    $("#confirm-send-task-atividade").val(key);
+    $("#confirm-send-task").modal("show");
+}
+
+
+function loadInfoSprint() {
+    findSprintFilhx(filhx.key, function (sprint) {
+        if (sprint.state == 0) {
+            $("#change-state-sprint").addClass("pattern-color-green");
+            $("#change-state-sprint i").attr("class", "fa fa-play fa-2x");
+        } else {
+            $("#change-state-sprint").addClass("pattern-color-red");
+            $("#change-state-sprint i").attr("class", "fa fa-stop fa-2x");
+        }
+        $("#timer").text(sprint.timer);
     });
 }
 
@@ -37,15 +82,25 @@ function renderTasks() {
         atividades.forEach(function (child) {
             var atividade = child.val();
             var at = $("<div></div>").addClass("_2hEQd _1E3L7").attr("id", "task_" + atividade.key);
-
+            if (atividade.estado == 1)
+                at.css("background-color", "#f9f9f9");
             var table = $("<table></table>").addClass("table-task");
 
             var tr = $("<tr></tr>").addClass("header-task");
             tr.append($("<td></td>").addClass("column1").append($("<span></span>").addClass("title-task").text(atividade.nome)));
 
             var td = $("<td></td>").addClass("column2");
-            td.append($("<a></a>").addClass("option-task").append($("<i></i>").addClass("fa fa-camera fa-2x")));
-            td.append($("<a></a>").addClass("option-task").append($("<i></i>").addClass("fa fa-comment-o fa-2x")));
+            var optiontask = $("<a></a>").addClass("option-task").append($("<i></i>").addClass("fa fa-camera fa-2x"));
+            if (atividade.estado == 1)
+                optiontask.attr("disabled", "disabled");
+            td.append(optiontask);
+
+            optiontask = $("<a></a>").addClass("option-task");
+            if (atividade.estado == 1)
+                optiontask.attr("disabled", "disabled").attr("href", "#");
+            else
+                optiontask.attr("href", "javascript:openModalComentario('" + atividade.key + "');");
+            td.append(optiontask.append($("<i></i>").addClass("fa fa-comment-o fa-2x")));
             tr.append(td);
             table.append(tr);
 
@@ -55,15 +110,13 @@ function renderTasks() {
             tr.append(td);
             td = $("<td></td>").addClass("column2");
             var btn = $("<button></button>").addClass("btn sendButton").text("Enviar ").append($("<i></i>").addClass("fa fa fa-send"));
-            btn.attr("onclick", "javascript:sendTask('" + atividade.key + "');");
-            if(atividade.estado == 1)
+            btn.attr("onclick", "javascript:openModalConfirmSendTask('" + atividade.key + "');");
+            if (atividade.estado == 1)
                 btn.attr("disabled", "disabled");
             td.append(btn);
             tr.append(td);
             table.append(tr);
             at.append(table);
-
-            console.log(at);
             $("#list-task").append(at);
         });
     });
